6783d952399de1aeba7b2039b73676d202cb46f4
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-04-14 19:21:22 +0000

    making some hard changes in the project

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
+let isSpeaking = false;
 
-window.speechSynthesis.onvoiceschanged = () =>{
+// Buttons
+let playBtn = document.getElementById("playBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let resumeBtn = document.getElementById("resumeBtn");
+let stopBtn = document.getElementById("stopBtn");
+
+window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice,i) => (voiceSelect.options[i] = new Option(voice.name , i)));
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 }
 
-voiceSelect.addEventListener("change",()=>{
+voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 })
 
-document.querySelector("button").addEventListener("click", ()=>{
+// Play button
+playBtn.addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
+    
+    // If speech is already in progress, cancel it and start fresh
+    if (isSpeaking) {
+        window.speechSynthesis.cancel();
+    }
+    
     window.speechSynthesis.speak(speech);
+    isSpeaking = true;
 })
+
+// Pause button
+pauseBtn.addEventListener("click", () => {
+    if (isSpeaking) {
+        window.speechSynthesis.pause();
+    }
+});
+
+// Resume button
+resumeBtn.addEventListener("click", () => {
+    if (isSpeaking) {
+        window.speechSynthesis.resume();
+    }
+});
+
+// Stop button
+stopBtn.addEventListener("click", () => {
+    if (isSpeaking) {
+        window.speechSynthesis.cancel();
+        isSpeaking = false;
+    }
+});
+
+// Update the speaking state when speech ends
+speech.onend = function() {
+    isSpeaking = false;
+};
